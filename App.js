352111f8092c4cb5cef456ad1cfa7ca172f91fa4
ashokmachineni <ashok5679m@gmail.com>
2020-06-24 11:14:52 +0100

    added dark theme with context api

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import "react-native-gesture-handler";
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React, { useMemo, useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 import {
   Provider as PaperProvider,
-  DarkTheme,
-  DefaultTheme
+  DarkTheme as DarkThemePaper,
+  DefaultTheme as DefaultThemePaper
 } from "react-native-paper";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DarkTheme as DarkThemeNavigation,
+  DefaultTheme as DefaultThemeNavigation
+} from "@react-navigation/native";
 import Navigation from "./src/navigation/Navigation";
+import PreferencesContext from "./src/context/PreferencesContext";
 
 export default function App() {
+  const [theme, setTheme] = useState("dark");
+  DefaultThemePaper.colors.primary = "#1ae1f2";
+  DarkThemePaper.colors.primary = "#1ae1f2";
+  DarkThemePaper.colors.accent = "#1ae1f2";
+  DarkThemeNavigation.colors.background = "#192734";
+  DarkThemeNavigation.colors.card = "#15212b";
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+  const preference = useMemo(() => ({ toggleTheme, theme }), [theme]);
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <Navigation />
-      </NavigationContainer>
-    </PaperProvider>
+    <PreferencesContext.Provider value={preference}>
+      <PaperProvider
+        theme={theme === "dark" ? DarkThemePaper : DefaultThemePaper}
+      >
+        <StatusBar
+          barStyle={theme === "dark" ? "light-content" : "dark-content"}
+        />
+        <NavigationContainer
+          theme={
+            theme === "dark" ? DarkThemeNavigation : DefaultThemeNavigation
+          }
+        >
+          <Navigation />
+        </NavigationContainer>
+      </PaperProvider>
+    </PreferencesContext.Provider>
   );
 }
---
+++ src/context/PreferencesContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const PreferencesContext = createContext({
+  theme: "",
+  toggleTheme: () => {}
+});
+export default PreferencesContext;
